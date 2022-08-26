df0a6c50002927d37c67856366108ec495c79529
George Rodriguez <govintor@icloud.com>
2022-08-25 21:22:50 -0400

    including the format date and render ejs

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.get('/',async (request, response)=>{
 })
 
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    db.collection('todos').insertOne({thing: request.body.todoItem, dueDate: request.body.addDate, completed: false
+    })
     .then(result => {
         console.log('Todo Added')
         response.redirect('/')
